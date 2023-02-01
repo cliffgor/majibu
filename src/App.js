da3841f954041ad03430b9baed60468bbec4301c
cliffgor <clierozigy@gmail.com>
2023-02-01 06:43:23 +0300

    Conditionals Added a block of code that would perform a a task given a condition

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ function App() {
         {id: 2, text: 'Wajibu wasemavyo two'},
         {id: 3, text: 'Wajibu wasemavyo three'}
     ]
+
+    const loading = false
+    const showComments = true
+    if(loading) return <h1>Loading....</h1>
     return(
         <div className="container">
             <h1>{title.toUpperCase()}</h1>
             <p>{body}</p>
-            <div>
+            {showComments && ( <div>
                 <h3>Comments ({comments.length})</h3>
                 <ul>
                     {comments.map((comment, index) => (
                         <li key={index}>{comment.text}</li>
                     ))}
                 </ul>
-            </div>
+            </div>)}
+           
         </div>
         
     )
