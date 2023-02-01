6e4403f4eafb78144fee6ab95c5d383e794fd595
cliffgor <clierozigy@gmail.com>
2023-02-01 06:03:55 +0300

    Expressions Created a dummy data to be passed into the app using different functioons and arrow functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 function App() {
+    const title = 'majibu Post'
+    const body = 'This is my jibu for the blog'
+    const comments = [
+        {id: 1, text: 'Wajibu wasemavyo one'},
+        {id: 2, text: 'Wajibu wasemavyo two'},
+        {id: 3, text: 'Wajibu wasemavyo three'}
+    ]
     return(
-        <div>
-            <h1>Welcome to Majibu</h1>
+        <div className="container">
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+            <div>
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index) => (
+                        <li key={index}>{comment.text}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
         
     )
